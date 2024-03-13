ff929256bc2d4b71b71f4849bf760ea19a1fc62b
vijaya <kodalivijayakumari12@gmail.com>
2024-03-13 23:25:47 +0530

    cimminting deep copy, using copyOfRange function

---
+++ deepCopy.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Arrays;
+//Performs deep copy, 
+// when the original array changes, 
+// the copied array doesn't change
+public class deepCopy {
+    public static void main(String[] args){
+        int numbers[] = {1,2,3,4,5};
+
+        int startingIndex = 1;
+        int endingIndex = 4;
+        int copyOfNumbers[] = Arrays.copyOfRange(numbers,startingIndex,endingIndex);
+
+        Arrays.fill(numbers, 0);
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(copyOfNumbers));
+        
+    }
+}
