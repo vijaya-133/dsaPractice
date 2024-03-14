08d5adf993f9c9a0d881947c570d98c4ed418614
vijaya <kodalivijayakumari12@gmail.com>
2024-03-14 22:59:36 +0530

    single inheritance

--- inheritance.java
+++ inheritance.java
@@ -3,6 +3,33 @@ import java.util.*;
 
 public class inheritance {
     public static void main(String[] args){
+        child c = new child();
+        System.out.println(c.b);
+        System.out.println(c.a);
+        //System.out.println(c.show());
+        
 
+    }
+}
+
+// Parent - Child
+//use keyword 'extends' for derived class
+
+//types -> single ingeritance
+//      -> multilevel inheritance
+//        ->multiple inheritance  -->interfaces
+//      ->heirachial inheritance
+
+class parent{
+    int a = 20;
+    void display(){
+        System.out.println("Parents");
+    }
+}
+
+class child extends parent{
+    int b = 10;
+    void show(){
+        System.out.println("Child");
     }
 }
