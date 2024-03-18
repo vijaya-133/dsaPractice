576e756143f6e421b9aeffd0ea0a884867e26095
vijaya <kodalivijayakumari12@gmail.com>
2024-03-18 22:38:59 +0530

    factorial finding

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+    // Recursive method to calculate factorial
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5; // Change this number to calculate factorial for a different number
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
